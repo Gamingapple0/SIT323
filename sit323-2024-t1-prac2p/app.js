92fab6e7f602bd4b566b976aa950b4233b7501fd
Anshu Madhikarmi <madhikarmianshu@gmail.com>
2024-03-02 16:31:24 +1100

    Update app.js

--- sit323-2024-t1-prac2p/app.js
+++ sit323-2024-t1-prac2p/app.js
@@ -1,10 +1,16 @@
-console.log("Week-1")
+// Print to the console
+console.log("Week-1");
 
-var express = require("express")
-var app = express()
+// Import the Express module
+var express = require("express");
 
+// Create an Express application
+var app = express();
+
+// Define a route that responds with "Hello World" when accessed with a GET request
 app.get("/", function (req, res){
-    res.send("Hello World")
-})
+    res.send("Hello World");
+});
 
-app.listen(3000)
+// Start the server on port 3000
+app.listen(3000);
